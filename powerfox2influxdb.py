8d0eac8c706d560bc28e745e561dd7ed9e354f0e
Christof Weigl <christof.weigl@gmail.com>
2022-02-12 15:56:32 +0100

    incomplete. maintenance at powerfox :(

--- powerfox2influxdb.py
+++ powerfox2influxdb.py
@@ -1,9 +1,36 @@
 import json
 import os
 import requests
-from requests.auth import HTTPBasicAuth
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 import time
 from datetime import datetime
 
 from influxdb import InfluxDBClient
+
+INFLUXDB_HOST = os.environ['INFLUXDB_HOST']
+INFLUXDB_PORT = os.environ['INFLUXDB_PORT']
+INFLUXDB_USER = os.environ['INFLUXDB_USER']
+INFLUXDB_PASS = os.environ['INFLUXDB_PASS']
+INFLUXDB_DB_NAME = os.environ['INFLUXDB_DB_NAME']
+
+POWERFOX_API = os.environ['POWERFOX_API']
+POWERFOX_USER = os.environ['POWERFOX_USER']
+POWERFOX_PASSWORD = os.environ['POWERFOX_PASSWORD']
+
+# POWERFOX_API=https://backend.powerfox.energy/api/2.0/my/main
+
+influx_client = InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT, username=INFLUXDB_USER, password=INFLUXDB_PASS)
+influx_client.switch_database(INFLUXDB_DB_NAME)
+
+local = datetime.now()
+timestamp = local.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+powerfox_headers = {'Content-Type': 'application/json'}
+powerfox_params = {'unit': 'kwh'}
+
+powerfox_response_current = requests.get(f"{POWERFOX_API}/current", params=powerfox_params, headers=powerfox_headers, auth=(POWERFOX_USER, POWERFOX_PASSWORD), verify=False)
+
+print(f"{timestamp} powerfox_response_current: {powerfox_response_current}")
